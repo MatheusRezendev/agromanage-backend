cdef96f17dda77f412cc461c5c34c13319be5701
Matheus Reginato Rezende <contatorzn@hotmail.com>
2025-05-03 17:55:12 -0300

    initial commit 🌱

---
+++ src/main/java/com/agromanage/agromanagebackend/AgromanageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.agromanage.agromanagebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgromanageBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgromanageBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agromanage/agromanagebackend/AgromanageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agromanage.agromanagebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgromanageBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
